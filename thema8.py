05c941a9a36e6edbd20d74b3beb49a6e1fde55a1
iliagou <ilia.gouma@hotmail.com>
2016-03-10 23:54:00 +0200

    New file

---
+++ thema8.py
@@ -0,0 +1,11 @@
+f = open('pinakas.txt')
+d = f.readlines()
+d = [x.split() for x in d]
+print "-----------arxikos pinakas------------"
+print ('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in d]))
+f.close()
+print "--------------------------------------"
+for i in range (3) :
+	d = list(reversed(zip(*d)))
+	print ('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in d]))
+	print "--------------------------------------"
